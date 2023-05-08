2b5130e056bd7e77435bbd301d5e997a85745f5c
NantenainaD12 <100203887+NantenainaD12@users.noreply.github.com>
2023-05-08 21:28:09 +0300

    ddd

--- resources/views/Liste_article.blade.php
+++ resources/views/Liste_article.blade.php
@@ -4,7 +4,6 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="shortcut icon" href="./img/fav.png" type="image/x-icon">
-  <link rel="stylesheet" href="https://kit-pro.fontawesome.com/releases/v5.12.1/css/pro.min.css">
   <link rel="stylesheet" type="text/css" href="template/css/style.css">
   <title>IA Information</title>
 </head>
